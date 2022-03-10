8eb0a2f49b329bcfa7499a4e557e49eb4b6f6c62
shanmugasundran1 <shanmugasundran1@gmail.com>
2022-03-11 04:46:22 +0800

    Initial Push

---
+++ components/CarItem/index.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { ImageBackground, Text, View } from 'react-native'
+import styles from './styles'
+
+import StyledButton from '../StyledButton'
+
+const CarItem = (props) => {
+  const { name, tagline, taglineCTA, image } = props.car
+
+  return (
+    <View style={styles.carContainer}>
+      <ImageBackground source={image} style={styles.image}>
+        <View style={styles.titles}>
+          <Text style={styles.title}>{name}</Text>
+          <Text style={styles.subtitle}>
+            {tagline} <Text style={styles.subtitleCTA}>{taglineCTA}</Text>
+          </Text>
+        </View>
+        <View style={styles.buttonContainer}>
+          <StyledButton
+            type='primary'
+            content={'Custom Order'}
+            onPress={() => {
+              console.log('Custom Order was pressed')
+            }}
+          />
+          <StyledButton
+            type='secondary'
+            content={'Existing Inventory'}
+            onPress={() => {
+              console.log('Existing Inventory was pressed')
+            }}
+          />
+        </View>
+      </ImageBackground>
+    </View>
+  )
+}
+
+export default CarItem
